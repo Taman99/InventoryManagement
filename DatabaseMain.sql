bcec1264a39e7c2884f07e80a0fa1e7a28eae2d9
Dishankdm <54790650+P5YTHE@users.noreply.github.com>
2022-01-06 11:49:31 +0530

    New Database

--- DatabaseMain.sql
+++ DatabaseMain.sql
@@ -1,37 +1,34 @@
-create database Products;
-create database Category;
-create database Size;
-
+create database InventoryManagement;
 
 --Run in Products db
 
-Create table tbl_Products (
-	pdt_id int identity(1,1) PRIMARY KEY,
-	pdt_name varchar(50),
-	user_id varchar(50),
-	pdt_desc varchar(200),
-	pdt_category_id int,
-	pdt_tag varchar(20),
-	pdt_quantity int,
-	pdt_price decimal,
-	pdt_discount decimal
+Create table Products (
+	ProductId int identity(1,1) PRIMARY KEY,
+	ProductName varchar(50),
+	MerchantId varchar(50),
+	ProductDesc varchar(200),
+	CategoryId int,
+	ProductTag varchar(20),
+	ProductQuantity int,
+	ProductPrice decimal,
+	ProductDiscount decimal
 );
-Create table tbl_ProductImages (
-	pdt_id int foreign key (pdt_id) REFERENCES tbl_Products(pdt_id),
-	image_url nvarchar(255), 
-	primary key (pdt_id,image_url)
+Create table ProductImages (
+	ProductId int foreign key (ProductId) REFERENCES Products(ProductId),
+	ImageUrl nvarchar(255), 
+	primary key (ProductId,ImageUrl)
 );
 
 --Run in Sizes Db
-create table tbl_Sizes(
-	size_index int PRIMARY KEY,
-	pdt_id int,
-	size varchar(10),
-	size_price decimal 
+create table Sizes(
+	SizeIndex int PRIMARY KEY,
+	ProductId int,
+	Size varchar(10),
+	SizePrice decimal 
 );
 
 --Run in Category db
-create table tbl_Categories(
-	category_id int identity(1,1) PRIMARY KEY,
-	category varchar(25),	
+create table Categories(
+	CategoryId int identity(1,1) PRIMARY KEY,
+	Category varchar(25),	
 );
