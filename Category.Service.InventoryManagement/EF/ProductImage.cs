03aa8dcbac60524d6cb2a7a1719c1831f4565208
Dishank Machkar <dishankdm@gmail.com>
2022-01-06 11:48:37 +0530

    DB Changes

---
+++ Category.Service.InventoryManagement/EF/ProductImage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Category.Service.EF
+{
+    public partial class ProductImage
+    {
+        public int ProductId { get; set; }
+        public string ImageUrl { get; set; } = null!;
+
+        public virtual Product Product { get; set; } = null!;
+    }
+}
