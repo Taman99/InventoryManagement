ffbc8b655e3c6cf79663df79e9f1140d9a725148
Taman <59500226+Taman99@users.noreply.github.com>
2022-01-15 17:56:08 +0530

    user specific category implemented

--- Category.Service.InventoryManagement/Entities/Category.cs
+++ Category.Service.InventoryManagement/Entities/Category.cs
@@ -1,16 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 
 namespace CategoryService.Entities
 {
     public partial class Category
     {
         public int CategoryId { get; set; }
-
-        [Required]
-        [MinLength(2)]
-        [MaxLength(50)]
         public string? CategoryName { get; set; }
+        public string? UserId { get; set; }
     }
 }
