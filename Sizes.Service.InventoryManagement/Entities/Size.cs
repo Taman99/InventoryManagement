54c2ddd1a089172bfbf8d7340b9d9ead8fe06577
Komal Patil <komap153@gmail.com>
2022-01-16 13:59:32 +0530

    Size api updated with productId as string

--- Sizes.Service.InventoryManagement/Entities/Size.cs
+++ Sizes.Service.InventoryManagement/Entities/Size.cs
@@ -6,7 +6,7 @@ namespace SizesService.Entities
     public partial class Size
     {
         public int SizeId { get; set; }
-        public int? ProductId { get; set; }
+        public string? ProductId { get; set; }
         public string? SizeName { get; set; }
         public decimal? SizePrice { get; set; }
     }
